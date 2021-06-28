154695ab87829df819dd2ccb14993e8cc86ab463
Lascoyle <meursault45@live.com>
2021-06-28 18:04:26 +0200

    Add a Navbar, Home and BlogList components. Learn how to pass props through a parent component to a child component, initate state component management

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import BlogList from './BlogList'
+
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: "My new website", body: "Lorem ipsum...", author: "Toad", id: 1 },
+        { title: "Welcome party!", body: "Lorem ipsum...", author: "Boo", id: 2 },
+        { title: "Web dev top tips", body: "Lorem ipsum...", author: "Wario", id: 3 },
+    ])
+
+    return (
+        <div className="home">
+            <BlogList blogs={blogs} title="All Blogs"></BlogList>
+        </div>
+     );
+}
+
+export default Home;
