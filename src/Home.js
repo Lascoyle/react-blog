e83c64fd87dca8c2dccfae70d6829e25392c80a1
Lascoyle <meursault45@live.com>
2021-06-28 18:14:09 +0200

    Re-use BlogList component with different datas by filtering blogs array only rendering Wario's blog

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,14 @@ const Home = () => {
         { title: "My new website", body: "Lorem ipsum...", author: "Toad", id: 1 },
         { title: "Welcome party!", body: "Lorem ipsum...", author: "Boo", id: 2 },
         { title: "Web dev top tips", body: "Lorem ipsum...", author: "Wario", id: 3 },
+        { title: "Poor boy, I'm a big mushroom head...", body: "Lorem ipsum...", author: "Toad", id: 3 },
+        { title: "It's holidays!", body: "Lorem ipsum...", author: "Wario", id: 5 },
     ])
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs"></BlogList>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Wario' )} title="Wario's Blogs"></BlogList>
         </div>
      );
 }
