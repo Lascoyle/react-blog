64a05e39402cbd07cf55867923e1d901f35a4417
Lascoyle <meursault45@live.com>
2021-06-29 17:02:22 +0200

    Re-use custom hooks for the BlogDetails component

---
+++ src/BlogDetails.js
@@ -0,0 +1,22 @@
+import useFetch from './useFetch'
+import { useParams } from "react-router-dom";
+
+const BlogDetails = () => {
+    const { id } = useParams();
+    const {data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
+
+    return (
+        <div className="blog-details">
+            {error && <div>{ error }</div>}
+            {isPending && <div>Loading...</div>}
+            {blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                    <div>{ blog.body }</div>
+                </article>
+            )}
+        </div>
+    );
+}
+export default BlogDetails;
