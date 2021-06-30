2d593ec4e9a72c18561d7477fbca6e9551ac743a
Lascoyle <meursault45@live.com>
2021-06-30 11:44:48 +0200

    Create the submit method for sending blog datas in json to the fake api

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,29 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('');
+    const [isPending, setIsPending] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        })
+            .then(() => {
+                console.log('new blog added');
+                setIsPending(false);
+            })
+    }
 
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title:</label>
                 <input type="text" required value={ title } onChange={(e) => setTitle(e.target.value)}/>
                 <label>Blog Body:</label>
@@ -18,7 +36,8 @@ const Create = () => {
                     <option value="Wario">Wario</option>
                     <option value="Boo">Boo</option>
                 </select>
-                <button>Add Blog</button>
+                { !isPending && <button>Add Blog</button>}
+                { isPending && <button disabled>Adding Blog...</button>}
                 <p>{ title }</p>
                 <p>{ body }</p>
                 <p>{ author }</p>
